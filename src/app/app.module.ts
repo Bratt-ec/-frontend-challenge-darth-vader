be649f46b56b1c8ba30edd0f04742cafc172d058
Bryan Castro <bry2098@gmail.com>
2022-05-06 21:03:43 -0500

    build and deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpInterceptorService } from './services/http-interceptor.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [CommonModule,BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [CommonModule,BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, { provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true },],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_key: 'M2RkYzZlOWEtYzU4OS00MGNlLWFkMjctNTE4ODFhNGI4MTYy',
-  base_url: 'https://api.m3o.com',
+  api_key: 'ac591438-fdc1-483c-80ba-d476c27ad03a',
+  base_url: 'https://holidayapi.com',
 };
